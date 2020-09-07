e8ce33aa28bb3d0a502dbf6c53d72c8d91e7b21b
Mart <you@example.com>
2020-09-07 11:36:32 +0200

    server

---
+++ client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import asyncio
+import logging
+import websockets
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)s:%(asctime)s - %(message)s', datefmt='%H:%M:%S')
+
+
+async def incoming_socket():
+    uri = "ws://192.168.2.20:8765"
+    async with websockets.connect(uri) as websocket:
+        while True:
+            message = await websocket.recv()
+            logging.info('%s', message)
+
+asyncio.get_event_loop().run_until_complete(incoming_socket())
