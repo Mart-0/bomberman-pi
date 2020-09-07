f56839d6038af58a5907ee6c7263cbbaff8fff32
Your Name <you@example.com>
2020-09-07 14:27:25 +0200

    server

--- client.py
+++ client.py
@@ -5,15 +5,19 @@ import asyncio
 import logging
 import websockets
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(levelname)s:%(asctime)s - %(message)s', datefmt='%H:%M:%S')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s:%(asctime)s - %(message)s",
+    datefmt="%H:%M:%S",
+)
 
 
 async def incoming_socket():
-    uri = config.server['host']
+    uri = config.server["host"]
     async with websockets.connect(uri) as websocket:
         while True:
             message = await websocket.recv()
-            logging.info('%s', message)
+            logging.info("%s", message)
+
 
 asyncio.get_event_loop().run_until_complete(incoming_socket())
