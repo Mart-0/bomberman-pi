136d7eb30c979dd1f46dbd12a7a4556f3f87bb04
Your Name <you@example.com>
2020-09-07 13:44:47 +0200

    server intial functions

--- server.py
+++ server.py
@@ -5,30 +5,67 @@ import json
 import logging
 import websockets
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(levelname)s:%(asctime)s - %(message)s', datefmt='%H:%M:%S')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s:%(asctime)s - %(message)s",
+    datefmt="%H:%M:%S",
+)
+
+
+class Chunk:
+    def __init__(self, position, grid):
+        self.position = position
+        self.grid = grid
+
 
 STATE = {"value": 0}
+
 USERS = set()
-
-
-def state_event():
-    return json.dumps({"type": "state", **STATE})
+BOMBS = set()
+CHUNKS = {
+    Chunk(
+        {"x": 1, "y": 1},
+        [
+            [0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0],
+        ],
+    )
+}
 
 
 def users_event():
     return json.dumps({"type": "users", "count": len(USERS)})
 
 
-async def notify_state():
-    if USERS:  # asyncio.wait doesn't accept an empty list
-        message = state_event()
-        await asyncio.wait([user.send(message) for user in USERS])
+def bombs_event():
+    return json.dumps({"type": "bombs", **BOMBS})
+
+
+def chunks_event():
+    return json.dumps({"type": "chunks", **CHUNKS})
 
 
 async def notify_users():
-    if USERS:  # asyncio.wait doesn't accept an empty list
+    if USERS:
         message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def notify_bombs():
+    if BOMBS:
+        message = bombs_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def notify_chunks():
+    if CHUNKS:
+        message = chunks_event()
         await asyncio.wait([user.send(message) for user in USERS])
 
 
@@ -46,15 +83,15 @@ async def incoming_socket(websocket, path):
     # register(websocket) sends user_event() to websocket
     await register(websocket)
     try:
-        await websocket.send(state_event())
+        await websocket.send(bombs_event())
         async for message in websocket:
             data = json.loads(message)
             if data["action"] == "minus":
                 STATE["value"] -= 1
-                await notify_state()
+                await notify_bombs()
             elif data["action"] == "plus":
                 STATE["value"] += 1
-                await notify_state()
+                await notify_bombs()
             else:
                 logging.error("unsupported event: %s", data)
     finally:
