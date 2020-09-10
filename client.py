447eb16a2b12279f77f630f663a53d3ad7e8b75d
Your Name <you@example.com>
2020-09-10 09:53:17 +0200

    server

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from sense_hat import SenseHat
-from multiprocessing import Process
 
 import config
 import asyncio
@@ -12,6 +11,7 @@ import threading
 import sys
 
 sense = SenseHat()
+sense.low_light = True
 
 logging.basicConfig(
     level=logging.INFO,
@@ -30,7 +30,7 @@ players = set()
 StartTime = time.time()
 
 inter = ""
-playerSpeed = 0.4
+playerSpeed = 0.3
 playerKeys = {"u": 0, "d": 0, "l": 0, "r": 0}
 anti_spam = 0
 running = 1
@@ -76,11 +76,197 @@ def place_bomb():
 async def send_bomb():
     global server, bombs, player
 
-    json_bomb = json.dumps({"action": "place_bomb", "data": {"position": player["position"]}})
+    json_bomb = json.dumps(
+        {"action": "place_bomb", "data": {"position": player["position"]}}
+    )
     await server.send(json_bomb)
     logging.info("%s", "bomb send")
 
 
+def draw_player():
+    if player:
+        sense.set_pixel(
+            player["position"]["x"], player["position"]["y"], player["color"]
+        )
+
+
+def move_screen(dir, r):
+    global player
+
+    if r == 0:
+        player["position"][dir.upper()] -= 1
+        player["position"][dir] = 7
+    else:
+        player["position"][dir.upper()] += 1
+        player["position"][dir] = 0
+
+
+def move_player(dir, r):
+    global player
+
+    new_position = player["position"].copy()
+
+    if r == 1:
+        if player["position"][dir] < 7:
+            new_position[dir] += 1
+        elif player["position"][dir] >= 7:
+            return move_screen(dir, r)
+    else:
+        if player["position"][dir] > 0:
+            new_position[dir] -= 1
+        elif player["position"][dir] <= 0:
+            return move_screen(dir, r)
+
+    s = check_position(new_position)
+
+    if s == 0 or s == 1:
+        player["position"] = new_position
+        asyncio.new_event_loop().run_until_complete(update_player())
+
+
+def check_position(position):
+    global chunks
+
+    index = ((position["y"]) * 8) + position["x"]
+
+    for chunk in chunks:
+        if (
+            chunk["position"]["x"] == position["X"]
+            and chunk["position"]["y"] == position["Y"]
+        ):
+            grid = chunk["grid"]
+
+    return grid[index]
+
+
+def check_keys():
+    global inter, playerKeys, anti_spam
+
+    if playerKeys == {"u": 0, "d": 0, "l": 0, "r": 0}:
+        anti_spam = 0
+        inter.cancel()
+
+    if playerKeys["u"] == 1:
+        move_player("y", 0)
+    if playerKeys["d"] == 1:
+        move_player("y", 1)
+    if playerKeys["l"] == 1:
+        move_player("x", 0)
+    if playerKeys["r"] == 1:
+        move_player("x", 1)
+
+
+def set_interval():
+    global inter, playerKeys, anti_spam
+    if anti_spam == 0:
+        inter = setInterval(playerSpeed, check_keys)
+
+
+def start_move(dir):
+    global playerKeys, anti_spam
+    playerKeys[dir] = 1
+    set_interval()
+    anti_spam = 1
+
+
+def move_up(event):
+    global player, anti_spam, playerKeys
+    if event.action == "pressed" and player["position"]["y"] >= 0:
+        start_move("u")
+    elif event.action == "released":
+        playerKeys["u"] = 0
+
+
+def move_down(event):
+    global player, anti_spam, playerKeys
+    if event.action == "pressed" and player["position"]["y"] <= 7:
+        start_move("d")
+    elif event.action == "released":
+        playerKeys["d"] = 0
+
+
+def move_left(event):
+    global player, anti_spam, playerKeys
+    if event.action == "pressed" and player["position"]["x"] >= 0:
+        start_move("l")
+    elif event.action == "released":
+        playerKeys["l"] = 0
+
+
+def move_right(event):
+    global player, anti_spam, playerKeys
+    if event.action == "pressed" and player["position"]["x"] <= 7:
+        start_move("r")
+    elif event.action == "released":
+        playerKeys["r"] = 0
+
+
+def stop():
+    global running, server
+    running = 0
+    sys.exit()
+
+
+sense.stick.direction_up = move_up
+sense.stick.direction_down = move_down
+sense.stick.direction_left = move_left
+sense.stick.direction_right = move_right
+sense.stick.direction_middle = place_bomb
+
+
+def show_bombs():
+    global player
+
+    if player and bombs:
+        bombs_in_screen = []
+        for bomb in bombs:
+            if (
+                bomb["position"]["X"] == player["position"]["X"]
+                and bomb["position"]["Y"] == player["position"]["Y"]
+            ):
+                bombs_in_screen.append(bomb)
+
+        for bomb in bombs_in_screen:
+            sense.set_pixel(bomb["position"]["x"], bomb["position"]["y"], R)
+
+
+def show_players():
+    global player
+
+    if player and players:
+        enemy_players = []
+        for enemy_player in players:
+            if (
+                enemy_player["position"]["X"] == player["position"]["X"]
+                and enemy_player["position"]["Y"] == player["position"]["Y"]
+            ):
+                enemy_players.append(enemy_player)
+
+        for player in enemy_players:
+            sense.set_pixel(
+                player["position"]["x"], player["position"]["y"], player["color"]
+            )
+
+
+def build_world():
+    global chunks
+    grid = 0
+
+    for chunk in chunks:
+        if (
+            chunk["position"]["x"] == player["position"]["X"]
+            and chunk["position"]["y"] == player["position"]["Y"]
+        ):
+            grid = chunk["grid"]
+
+    if grid:
+        O = (0, 0, 0)
+        PW = (200, 200, 255)
+        TW = (100, 48, 48)
+
+        dic = {0: O, 1: O, 2: TW, 3: PW}
+        pixels = [dic.get(n, n) for n in grid]
+        sense.set_pixels(pixels)
 
 
 class WebsocketThread(threading.Thread):
@@ -125,151 +311,6 @@ async def incoming_socket():
                 logging.error("unsupported event: %s", data)
 
 
-def draw_player():
-    if player:
-        sense.set_pixel(
-            player["position"]["x"], player["position"]["y"], player["color"]
-        )
-
-
-def start_move(dir):
-    global playerKeys, anti_spam
-    playerKeys[dir] = 1
-    set_interval()
-    anti_spam = 1
-
-
-def move_player(dir, r):
-    global player
-
-    new_position = player["position"].copy()
-
-    if r == 1:
-        if player["position"][dir] < 7:
-            new_position[dir] += 1
-    else:
-        if player["position"][dir] > 0:
-            new_position[dir] -= 1
-    s = check_position(new_position["x"], new_position["y"])
-
-    if s == 0 or s == 1:
-        player["position"] = new_position
-        asyncio.new_event_loop().run_until_complete(update_player())
-        
-
-def check_position(x, y):
-    global chunks
-
-    index = ((y) * 8) + x
-
-    i = 0
-    for chunk in chunks:
-        if i == 0:
-            grid = chunk["grid"]
-        i += 1
-
-    return grid[index]
-
-
-def check_keys():
-    global inter, playerKeys, anti_spam
-
-    if playerKeys == {"u": 0, "d": 0, "l": 0, "r": 0}:
-        anti_spam = 0
-        inter.cancel()
-
-    if playerKeys["u"] == 1:
-        move_player("y", 0)
-    if playerKeys["d"] == 1:
-        move_player("y", 1)
-    if playerKeys["l"] == 1:
-        move_player("x", 0)
-    if playerKeys["r"] == 1:
-        move_player("x", 1)
-
-
-def set_interval():
-    global inter, playerKeys, anti_spam
-    if anti_spam == 0:
-        inter = setInterval(playerSpeed, check_keys)
-
-
-def move_up(event):
-    global player, anti_spam, playerKeys
-    if event.action == "pressed" and player["position"]["y"] > 0:
-        start_move("u")
-    elif event.action == "released":
-        playerKeys["u"] = 0
-
-
-def move_down(event):
-    global player, anti_spam, playerKeys
-    if event.action == "pressed" and player["position"]["y"] < 7:
-        start_move("d")
-    elif event.action == "released":
-        playerKeys["d"] = 0
-
-
-def move_left(event):
-    global player, anti_spam, playerKeys
-    if event.action == "pressed" and player["position"]["x"] > 0:
-        start_move("l")
-    elif event.action == "released":
-        playerKeys["l"] = 0
-
-
-def move_right(event):
-    global player, anti_spam, playerKeys
-    if event.action == "pressed" and player["position"]["x"] < 7:
-        start_move("r")
-    elif event.action == "released":
-        playerKeys["r"] = 0
-
-
-def stop():
-    global running, server
-    running = 0
-    sys.exit()
-
-
-sense.stick.direction_up = move_up
-sense.stick.direction_down = move_down
-sense.stick.direction_left = move_left
-sense.stick.direction_right = move_right
-sense.stick.direction_middle = place_bomb
-
-
-def show_bombs():
-    for bomb in bombs:
-        sense.set_pixel(bomb["position"]["x"], bomb["position"]["y"], R)
-
-
-def show_players():
-    for player in players:
-        sense.set_pixel(
-            player["position"]["x"], player["position"]["y"], player["color"]
-        )
-
-
-def build_world():
-    global chunks
-    grid = 0
-    i = 0
-    for chunk in chunks:
-        if i == 0:
-            grid = chunk["grid"]
-        i += 1
-
-    if grid:
-        O = (0, 0, 0)
-        PW = (140, 140, 200)
-        TW = (100, 48, 48)
-
-        dic = {0: O, 1: O, 2: TW, 3: PW}
-        pixels = [dic.get(n, n) for n in grid]
-        sense.set_pixels(pixels)
-
-
 server = WebsocketThread()
 server.start()
 
@@ -277,10 +318,10 @@ server.start()
 def game_loop():
     while running:
         build_world()
-        show_players()
         show_bombs()
+        show_players()
         draw_player()
-        time.sleep(0.1)
+        time.sleep(0.05)
         sense.clear(0, 0, 0)
 
 
